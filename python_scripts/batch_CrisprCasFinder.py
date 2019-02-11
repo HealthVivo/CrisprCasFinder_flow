e312f6157dc5efb26cbfe969197613b064f3d4d1
Ian Rambo <ian@dhcp-146-6-184-232.utmsi.utexas.edu>
2019-02-10 20:45:37 -0600

    msfThreads constant

--- python_scripts/batch_CrisprCasFinder.py
+++ python_scripts/batch_CrisprCasFinder.py
@@ -69,7 +69,7 @@ parser.add_argument('--msfThreads', type=int, dest='msfThreads', action='store',
 parser.add_argument('--projectName', type=str, dest='projectName', action='store', const='', nargs = '?', help='project name for output directories')
 parser.add_argument('--hashAlg', type=str, dest='hashAlg', action='store', const='sha256', nargs = '?', help='hash algorithm to use for testing gzip integrity. Choose from: sha1, sha224, sha256, sha384, sha512, md5. Default == sha256')
 opts = parser.parse_args()
-print(opts.nJobs)
+print(opts.nJobs[0])
 #==============================================================================
 #Command line options for CrisprCasFinder
 ccfinder_opts = {'-log':'', '-copyCSS':'', '-repeats':'', '-DBcrispr':'',

0db35058bc76376399fe65d3ba6da2aa35cefd76
Ian Rambo <ian@dhcp-146-6-184-232.utmsi.utexas.edu>
2019-02-10 20:25:31 -0600

    logging

--- python_scripts/batch_CrisprCasFinder.py
+++ python_scripts/batch_CrisprCasFinder.py
@@ -5,7 +5,7 @@ import argparse
 import subprocess
 import hashlib
 import magic
-from warnings import warn
+import logging
 
 '''
 Ian Rambo
@@ -13,6 +13,8 @@ February 9, 2019
 Purpose:
 Wrapper script for running multiple batch loops of crisprCasFinder
 in parallel via GNU Parallel.
+
+Thirteen... that's a mighty unlucky number... for somebody!
 '''
 #------------------------------------------------------------------------------
 def optstring_join(optdict):
@@ -34,6 +36,25 @@ def file_as_blockiter(afile, blocksize=65536):
             yield block
             block = afile.read(blocksize)
 #------------------------------------------------------------------------------
+def hash_sum(g, alg):
+    valid_algs = ['sha1', 'sha224', 'sha256', 'sha384', 'sha512', 'md5']
+    if alg and alg == 'sha1':
+        hash = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha1())
+    if alg and alg == 'sha224':
+        hash = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha224())
+    if alg and alg == 'sha256':
+        hash = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha256())
+    if alg and alg == 'sha384':
+        hash = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha384())
+    if alg and alg == 'sha512':
+        hash = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha512())
+    if alg and alg == 'md5':
+        hash = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.md5())
+    elif alg and not alg in valid_algs:
+        print('choose a valid algorithm from: %s' % ','.join(valid_algs))
+
+    return hash
+#------------------------------------------------------------------------------
 #Paths to CrisprCasFinder perl script and sofile
 ccfinder_path = '/home/rambo/bin/CRISPRCasFinder/CRISPRCasFinder.pl'
 ccfinder_sofile = '/home/rambo/bin/CRISPRCasFinder/sel392v2.so'
@@ -46,6 +67,7 @@ parser.add_argument('--genomeDir', type=str, dest='genomeDir', action='store', n
 parser.add_argument('--workingDir', type=str, dest='workingDir', action='store', nargs=1, help='working directory')
 parser.add_argument('--msfThreads', type=int, dest='msfThreads', action='store', nargs=1, const=1, nargs='?', help='MacSyFinder threads')
 parser.add_argument('--projectName', type=str, dest='projectName', action='store', nargs=1, const='', help='project name for output directories')
+parser.add_argument('--hashAlg', type=str, dest='hashAlg', action='store', nargs=1, const='sha256', help='hash algorithm to use for testing gzip integrity. Choose from: sha1, sha224, sha256, sha384, sha512, md5. Default == sha256')
 opts = parser.parse_args()
 #==============================================================================
 #Command line options for CrisprCasFinder
@@ -56,7 +78,6 @@ ccfinder_opts = {'-log':'', '-copyCSS':'', '-repeats':'', '-DBcrispr':'',
 '-quiet':'', '-soFile':ccfinder_sofile, '-keep':''}
 ccfinder_optstring = optstring_join(ccfinder_opts)
 print('CRISPRCasFinder options:\n#=============\n%s\n#=============' % ccfinder_optstring)
-
 #==============================================================================
 globString = '%s/*.%s' % (opts.genomeDir, opts.ending)
 
@@ -86,50 +107,58 @@ analyzed = 0
 for g in genomes_to_run :
     #Is file gzip compressed?
     if magic.from_file(g).startswith('gzip compressed data') and g.endswith('.gz'):
-        #Get sha256 hash for gzipped file
-        gz0_sha256 = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha256())
+        #Get hash for gzipped file
+        hash0 = hash_sum(g, opts.hashAlg)
 
         try:
-            gunzip_cmd = 'gunzip %s' % gunzip_file
+            gunzip_cmd = 'gunzip %s' % g
             gunzip_proc = subprocess.Popen(gzip_cmd, shell = True)
         except subprocess.CalledProcessError as gunzipexc:
-            print("error code", gunzipexc.returncode, gunzipexc.output)
-
-        gunzip_file = os.path.splitext(g)[0]
-        print('running CrisprCasFinder for %s' % gunzip_file)
-        ccfind_cmd = 'perl %s -in %s %s'  % (ccfinder_path, gunzip_file, ccfinder_optstring)
-        subprocess.call(ccfind_cmd, shell = True)
+            logging.error("error code", gunzipexc.returncode, gunzipexc.output)
+        try:
+            #run CrisprCasFinder for gunzipped file
+            gunzip_file = os.path.splitext(g)[0]
+            logging.info('running CrisprCasFinder for %s' % gunzip_file)
+            ccfind_cmd = 'perl %s -in %s %s'  % (ccfinder_path, gunzip_file, ccfinder_optstring)
+            subprocess.call(ccfind_cmd, shell = True)
+        except subprocess.CalledProcessError as ccfindexc:
+            logging.error("error code", ccfindexc.returncode, ccfindexc.output)
         analyzed += 1
         #Re-gzip the file
         try:
             gzip_cmd = 'gzip ' + gunzip_file + ' &'
             gzip_proc = subprocess.Popen(gzip_cmd, shell = True)
         except subprocess.CalledProcessError as gzipexc:
-            print("error code", gzipexc.returncode, gzipexc.output)
+            logging.error("error code", gzipexc.returncode, gzipexc.output)
         #Test the gzip compression
         try:
-            print('testing gzip compression')
+            logging.info('testing gzip compression')
             gztest_cmd = 'gzip -t %s' % g
             gztest_proc = subprocess.Popen(gztest_cmd, shell = True)
             streamdata = gztest_proc.communicate()[0]
             rc = proc.returncode
             if rc == 0:
-                print('gzip compression successful')
+                logging.info('gzip compression successful')
         except subprocess.CalledProcessError as gztestexc:
-            print("error code", gztestexc.returncode, gztestexc.output)
+            logging.error("error code", gztestexc.returncode, gztestexc.output)
 
-        #Get sha256 hash for re-gzipped file
-        gz1_sha256 = hash_bytestr_iter(file_as_blockiter(open(g, 'rb')), hashlib.sha256())
+        #Get hash for re-gzipped file
+        hash1 = hash_sum(g, opts.hashAlg)
 
         #Do the hashes match?
-        if gz0_sha256 == gz1_sha256:
+        if hash0 == hash1
             continue
         else:
-            warn('sha256 hashes do not match for %s' % g)
+            wmessage = 'WARNING: file %s may be damaged. %s hashes do not match for gzipped files' % (g, opts.hashAlg)
+            logging.warning(wmessage)
 
     else:
-        ccfind_cmd = 'perl %s -in %s %s'  % (ccfinder_path, g, ccfinder_optstring)
-        subprocess.call(ccfind_cmd, shell = True)
+        try:
+            logging.info('running CrisprCasFinder for %s' % g)
+            ccfind_cmd = 'perl %s -in %s %s'  % (ccfinder_path, g, ccfinder_optstring)
+            subprocess.call(ccfind_cmd, shell = True)
+        except subprocess.CalledProcessError as ccfindexc:
+            logging.error("error code", ccfindexc.returncode, ccfindexc.output)
         analyzed += 1
 
-    print('analysis complete for genome %d of %d, batch %d' % (analyzed, len(genomes_to_run), opts.jobID))
+    logging.info('analysis complete for genome %d of %d, batch %d' % (analyzed, len(genomes_to_run), opts.jobID))
